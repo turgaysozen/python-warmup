07a73bec53331604da6febe99643b665a6a66045
turgaysozen <turgaysozen@hotmail.com>
2021-03-12 16:52:05 +0300

    initial commit

--- fibonacci_by_generator.py
+++ fibonacci_by_generator.py
@@ -1,14 +1,27 @@
+class Python_For_Fun:
+    def factoriel(self, n):
+        if n <= 1:
+            return 1
+        else:
+            return self.factoriel(n - 1) * n
 
-class Fibonacci:
-    def fibonacci(self):
+    def print_factoriel_result(self, n):
+        print(self.factoriel(n))
+
+    def fibonacci(self, n):
         a, b = 0, 1
-        counter = 1
-        while counter < 15:
-            yield print(a)
+        counter = 0
+        fibonacci_list = []
+        while counter <= n:
+            fibonacci_list.append(a)
             a, b = b, a + b
             counter += 1
+        return fibonacci_list
+
+    def print_fibonacci(self, n):
+        print(self.fibonacci(n))
 
 
-fi = Fibonacci().fibonacci()
-for _ in range(0, 25):
-    fi.__next__()
+python_for_fun = Python_For_Fun()
+python_for_fun.print_factoriel_result(6)
+python_for_fun.print_fibonacci(30000)
---
+++ variable_type.py
@@ -0,0 +1,20 @@
+# class Car:
+#     def __init__(self, model, engine, fuel):
+#         self.model = model
+#         self.engine = engine
+#         self.fuel = fuel
+#
+#     def do_rent(*, rent):
+#         print(*, rent)
+#
+#
+# car = Car(2021, 'V8', 'Gasoline')
+# # print(car.__dict__)
+# print(car.do_rent(1, 2, rent='dede'))
+
+tt = None
+if tt is None:
+    raise Exception('error')
+
+
+print('yeahhh')
